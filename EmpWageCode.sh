2b0293f41783b6f79e6ae952e7bba9448f7f1740
pratik1312 <patilpratik194@gmail.com>
2020-09-29 14:59:13 +0530

    Added: file shows the refactoring the code using function

--- EmpWageCode.sh
+++ EmpWageCode.sh
@@ -7,9 +7,9 @@ TOTAL_WAGE=0
 PER_MONTH_WAGE=0
 DAY=1
 HOURS=0
-while (( $(($DAY<21)) && $(($HOURS<=100)) ))
-do
-        check=$(( RANDOM%3 ))
+GetWorkHours()
+{
+#        check=$(( RANDOM%3 ))
         case $check in
         1)
 #               echo "Employee Present for full time"
@@ -21,6 +21,11 @@ do
 #               echo "Absent"
                 emphrs=0;;
 esac
+}
+while (( $(($DAY<21)) && $(($HOURS<=100)) ))
+do
+        check=$(( RANDOM%3 ))
+        GetWorkHours $check
         HOURS=$(( $HOURS + $emphrs ))
         TOTAL_WAGE=$(( $emphrs * $WAGE_PER_HOUR ))
         PER_MONTH_WAGE=$(( $TOTAL_WAGE + $PER_MONTH_WAGE ))
@@ -28,3 +33,4 @@ DAY=$((DAY+1))
 done
 echo "Month wages : " $PER_MONTH_WAGE
 echo "Total hours : " $HOURS
+
