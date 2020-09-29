b6e015d05d757613c3c688767bf807de5b33013c
pratik1312 <patilpratik194@gmail.com>
2020-09-29 11:40:21 +0530

    Added:File calculates the Daily Wage of Employee

--- EmpWageCode.sh
+++ EmpWageCode.sh
@@ -1,10 +1,16 @@
-echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash -x
-isPresent=1
+echo "Welcome to Employee Wage Computation Program"
 check=$(( RANDOM%2 ))
-if [ $isPresent -eq $check ]
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+TOTAL_WAGE=0
+if [ $check -eq 0 ]
 then
 	echo "Employee Present"
+	TOTAL_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
+	echo "Total Wage : " $TOATL_WAGE
 else
 	echo "Employee Absent"
+	echo "Total Wage : " $TOTAL_WAGE
 fi
+
