81ac0427535ca6a03ad19347294d6732e1d5503a
pratik1312 <patilpratik194@gmail.com>
2020-09-29 12:13:15 +0530

    Added:file computes the wages for a month

--- EmpWageCode.sh
+++ EmpWageCode.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-check=$(( RANDOM%3 ))
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME=4
 TOTAL_WAGE=0
-case $check in
+PER_MONTH_WAGE=0
+DAY=1
+while [ $DAY -ne 20 ]
+do
+	check=$(( RANDOM%3 ))
+	case $check in
 	1)
-		echo "Employee Present for full time"
+#		echo "Employee Present for full time"
 		emphrs=8;;
 	2)
-		echo "Employee Present for part time"
+#		echo "Employee Present for part time"
 		emphrs=4;;
 	*)
-		echo "Absent"
+#		echo "Absent"
 		emphrs=0;;
 esac
-TOTAL_WAGE=$(( $emphrs * $WAGE_PER_HOUR ))
-echo $TOTAL_WAGE
-
+	TOTAL_WAGE=$(( $emphrs * $WAGE_PER_HOUR ))
+	PER_MONTH_WAGE=$(( $TOTAL_WAGE + $PER_MONTH_WAGE ))
+DAY=$((DAY+1))
+done
+echo "Month wages : " $PER_MONTH_WAGE
