2d5d5b7da5dbd2529e00443384b7868d811d0e51
pratik1312 <patilpratik194@gmail.com>
2020-09-29 11:19:13 +0530

    Added:file checks for the employee presence

--- EmpWageCode.sh
+++ EmpWageCode.sh
@@ -1,1 +1,10 @@
 echo "Welcome to Employee Wage Computation Program"
+#!/bin/bash -x
+isPresent=1
+check=$(( RANDOM%2 ))
+if [ $isPresent -eq $check ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
