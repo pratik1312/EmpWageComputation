632f1d6cd0a0843eb460045be47100313907fabb
pratik1312 <patilpratik194@gmail.com>
2020-09-29 11:50:57 +0530

    Added:file calculates the daily wages for part time employee

--- EmpWageCode.sh
+++ EmpWageCode.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-check=$(( RANDOM%2 ))
+check=$(( RANDOM%3 ))
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
+PART_TIME=4
 TOTAL_WAGE=0
 if [ $check -eq 0 ]
 then
-	echo "Employee Present"
+	echo "Employee Present for full time"
 	TOTAL_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	echo "Total Wage : " $TOATL_WAGE
+	echo "Total Wage : " $TOTAL_WAGE
+elif [ $check -eq 1 ]
+	echo "Employee Present for part time"
+	TOTAL_WAGE=$(($WAGE_PER_HOUR * $PART_TIME))
+	echo "Total Wage : " $TOTAL_WAGE
 else
 	echo "Employee Absent"
 	echo "Total Wage : " $TOTAL_WAGE
